b4db109b632ba6a77d9ad96abe3a3c12c86e00e0
mdelaneycastle <mdelaney@castlefineart.com>
2025-06-23 22:49:03 +0100

    newupdate

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,6 @@
-// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZpqSGl5sJrCMDfKIbwpLMO3pdvm6q218",
@@ -13,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
