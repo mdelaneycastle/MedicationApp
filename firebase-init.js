d69070246f7c1be8e2889ef9b755b49d42fc724e
mdelaneycastle <154420982+mdelaneycastle@users.noreply.github.com>
2025-06-26 15:12:57 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,3 @@
-// firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCZpqSGl5sJrCMDfKIbwpLMO3pdvm6q218",
   authDomain: "medsreminder-cbc03.firebaseapp.com",
@@ -12,10 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "907426014383",
   appId: "1:907426014383:web:b798cb8806f3f606b3605a"
 };
-
-// Init Firebase
-const app = initializeApp(firebaseConfig);
-
-// ✅ Export both auth and db
-export const auth = getAuth(app);
-export const db = getFirestore(app);
